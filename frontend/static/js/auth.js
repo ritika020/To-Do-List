97c456d2ac5a1bb36e082e5a259951cc09d26ca3
ritika020 <ritikakhandelwal312@gmail.com>
2024-11-15 07:32:39 -0500

    Registration solved

--- frontend/static/js/auth.js
+++ frontend/static/js/auth.js
@@ -1,4 +1,4 @@
-const API_URL = 'http://localhost:5000';
+const API_URL = 'http://127.0.0.1:5000';
 
 // Login functionality
 document.getElementById('loginForm')?.addEventListener('submit', async (e) => {
